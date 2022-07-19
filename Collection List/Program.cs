7799eac69df6aba01bbc288d37a77fb748f5493c
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-07-20 01:13:13 +0530

    Add project files.

---
+++ Collection List/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Collection_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // first  type
+            List<int> obj = new List<int>();
+            obj.Add(12);
+            obj.Add(123);
+            obj.Add(124);
+            obj.Add(125);
+
+            foreach(int i in obj)
+            {
+                Console.WriteLine(i);
+            }
+            // second type 
+            List<string> s = new List<string>() { "akshay  ", "saini   ", "hiii   ", "hello   " };
+            foreach (string item in s)
+            {
+                Console.Write(item);
+            }
+
+            Console.WriteLine(obj.Count);
+        }
+    }
+}
